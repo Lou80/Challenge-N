c9b0f15fc72ba7be4952e5fd3a66fdcece8d4ab4
Lou80 <lourdesramirez@yahoo.com>
2019-08-11 19:26:09 -0300

    slides y modal

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const sqMeters = document.getElementById('sq_meters');
 const sqMeterPrice = document.getElementById('sq_meter_price');
 const heart = document.querySelector('#like');
 localStorage.getItem('like') ? heart.classList.add('red') : '';
+const form = document.getElementById('form');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
 
 const format = () => {
 price.innerHTML = parseInt( localStorage.getItem('saved_price') )
@@ -34,5 +37,37 @@ const setLike = () => {
 }
 
 const openForm = () => {
+    form.classList.remove('hidden');
+    main.classList.add('opacity');
+    
+}
 
+const closeForm = () => {
+   main.classList.remove('opacity');
+
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  
+  slides[slideIndex-1].style.display = "block"; 
+  
 }
