b6b673696d9552a25cc40008614e385431778c74
Lou80 <lourdesramirez@yahoo.com>
2019-08-11 15:07:37 -0300

    form

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const price = document.getElementById('price');
 localStorage.getItem('saved_price') ? '' : localStorage.setItem('saved_price', '1400000');
 const sqMeters = document.getElementById('sq_meters');
+const sqMeterPrice = document.getElementById('sq_meter_price');
 const heart = document.querySelector('#like');
 localStorage.getItem('like') ? heart.classList.add('red') : '';
 
@@ -13,7 +14,6 @@ format();
 
 const calculate = () => {
     const result = localStorage.getItem('saved_price') / sqMeters.innerHTML;
-    const sqMeterPrice = document.getElementById('sq_meter_price');
     if (result >= 1) {
         sqMeterPrice.innerHTML = parseInt(result);
     } else {
@@ -31,4 +31,8 @@ const setStorage = () => {
 const setLike = () => {
     heart.classList.toggle('red');
     localStorage.getItem('like') ? localStorage.removeItem('like') : localStorage.setItem('like', 'red');
+}
+
+const openForm = () => {
+
 }
